f812631b9f27b76c9daffc530c2479f7d74d863a
Rebun <rubengdopazo@gmail.com>
2023-01-27 09:31:56 +0000

    Starting all over again Created project from scratch Ruben Gomez Dopazo

--- src/experience.ts
+++
@@ -1,4 +0,0 @@
-import { createApp } from 'vue'
-import Experience from './Experience.vue'
-
-createApp(Experience).mount('#app') //mounts the app Experience inside the id=app div
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { createApp } from 'vue'
-import Index from './Index.vue'
-
-createApp(Index).mount('#app') //mounts the app Index inside the id=app div
