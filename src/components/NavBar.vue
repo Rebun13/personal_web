b3f1200c99fbaef26f9f53eeefe1b550387d6816
Rebun <55385643+Rebun13@users.noreply.github.com>
2023-01-29 10:54:44 +0000

    Style changes and image compression Images must be 244kB or smaller. Ruben Gomez Dopazo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,16 +10,25 @@ defineProps({
 </script>
 
 <template>
-  <nav class="relative w-full flex flex-wrap py-3 bg-gray-700 text-gray-200 shadow-lg">
+  <nav
+    class="relative w-full flex flex-wrap py-3 bg-gray-700 text-gray-200 shadow-lg"
+  >
     <div class="w-full flex flex-wrap items-center justify-between px-6 h-7">
       <ul>
-        <li id="navMe" class="display: inline-block px-4 hover:text-teal-400 text-lime-400"><a href="index.html">About
-            me</a></li>
-        <li id="navXp" class="display: inline-block px-4 hover:text-teal-400"><a href="experience.html">My working
-            experience</a></li>
-        <li id="navSt" class="display: inline-block px-4 hover:text-teal-400"><a href="studies.html">My studies</a>
+        <li
+          id="navMe"
+          class="display: inline-block px-4 hover:text-teal-400 text-lime-400"
+        >
+          <a href="index.html">About me</a>
         </li>
-        <li id="navPr" class="display: inline-block px-4 hover:text-teal-400"><a href="projects.html">My projects</a>
+        <li id="navXp" class="display: inline-block px-4 hover:text-teal-400">
+          <a href="experience.html">My working experience</a>
+        </li>
+        <li id="navSt" class="display: inline-block px-4 hover:text-teal-400">
+          <a href="studies.html">My studies</a>
+        </li>
+        <li id="navPr" class="display: inline-block px-4 hover:text-teal-400">
+          <a href="projects.html">My projects</a>
         </li>
       </ul>
       <img class="h-full" src="/images/logo1-white.png" alt="logo" />
