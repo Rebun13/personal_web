505f7d218e23ccac1404d61714f48fbaefd07b0a
Rebun <55385643+Rebun13@users.noreply.github.com>
2023-02-02 10:44:41 +0000

    Added background image New background pattern for all pages. Ruben Gomez Dopazo

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,45 @@ import FooterBar from "./components/FooterBar.vue";
 </script>
 
 <template>
-  <div
-    id="nav"
-    class="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-700 text-gray-200 shadow-lg"
-  >
-    <div class="w-full flex flex-wrap items-center justify-between px-6 h-7">
-      <RouterLink class="display: inline-block px-4 hover:text-teal-400" to="/"
-        >Home</RouterLink
-      >
-      <RouterLink
-        class="display: inline-block px-4 hover:text-teal-400"
-        to="/aboutMe"
-        >About me</RouterLink
-      >
-      <RouterLink
-        class="display: inline-block px-4 hover:text-teal-400"
-        to="/experience"
-        >Experience</RouterLink
-      >
-      <RouterLink
-        class="display: inline-block px-4 hover:text-teal-400"
-        to="/studies"
-        >Studies</RouterLink
-      >
-      <RouterLink
-        class="display: inline-block px-4 hover:text-teal-400"
-        to="/projects"
-        >Projects</RouterLink
-      >
-      <img class="h-full" src="/images/logo1-white.png" alt="logo" />
+  <div class="bg-bg-pattern">
+    <div
+      id="nav"
+      class="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-700 text-gray-200 shadow-lg"
+    >
+      <div class="w-full flex flex-wrap items-center justify-between px-6 h-7">
+        <RouterLink
+          class="display: inline-block px-4 hover:text-teal-400"
+          to="/"
+          >Home</RouterLink
+        >
+        <RouterLink
+          class="display: inline-block px-4 hover:text-teal-400"
+          to="/aboutMe"
+          >About me</RouterLink
+        >
+        <RouterLink
+          class="display: inline-block px-4 hover:text-teal-400"
+          to="/experience"
+          >Experience</RouterLink
+        >
+        <RouterLink
+          class="display: inline-block px-4 hover:text-teal-400"
+          to="/studies"
+          >Studies</RouterLink
+        >
+        <RouterLink
+          class="display: inline-block px-4 hover:text-teal-400"
+          to="/projects"
+          >Projects</RouterLink
+        >
+        <img class="h-full" src="/images/logo1-white.png" alt="logo" />
+      </div>
     </div>
-  </div>
-  <div class="container">
-    <RouterView></RouterView>
-  </div>
-  <div id="footer">
-    <FooterBar />
+    <div class="container">
+      <RouterView></RouterView>
+    </div>
+    <div id="footer">
+      <FooterBar />
+    </div>
   </div>
 </template>
