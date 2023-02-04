fe4eb4c22a00bceeede4f84271b673f034686997
Rebun <55385643+Rebun13@users.noreply.github.com>
2023-02-04 20:13:09 +0000

    Updated studies view First version of the content. Ruben Gomez Dopazo

--- src/views/StudiesView.vue
+++ src/views/StudiesView.vue
@@ -1,5 +1,67 @@
 <template>
-  <div id="studies">
-    <h1>Studies</h1>
+  <div id="studies" class="lg:w-3/5 mx-auto mt-16 md:w-fullg">
+    <div
+      id="studies-1"
+      class="mb-16 grid grid-cols-2 gap-0 items-center bg-gray-700 shadow-l"
+    >
+      <div id="s1-profile_picture-container" class="mx-auto">
+        <img src="/images/holder.jpeg" alt="profile_picture" />
+      </div>
+      <div id="s1-text-container" class="mx-6 py-8 text-gray-200">
+        <div id="s1-text-title" class="mb-8">
+          <h1 class="text-lime-400 text-4xl">Mechanical Engineering</h1>
+          <h3 class="text-2xl">2013 - 2016 (not finished)</h3>
+        </div>
+        <p class="text-justify">
+          The first time I entered the university, I opted for Mechanical
+          Engineering. I was interested in motorsports, so my objective was to
+          end up designing race cars. However, in my third year I understood
+          that I had not enough motivation, and I left the degree.<br />During
+          these three years I learnd a lot about maths and physics, two subjects
+          I love.
+        </p>
+      </div>
+    </div>
+    <div
+      id="studies-2"
+      class="mb-16 grid grid-cols-2 gap-0 items-center bg-gray-700 shadow-l"
+    >
+      <div id="s2-text-container" class="mx-6 py-8 text-gray-200">
+        <div id="s2-text-title" class="mb-8">
+          <h1 class="text-lime-400 text-4xl">Telecommunications Engineering</h1>
+          <h3 class="text-2xl">2017 - 2018 (not finished)</h3>
+        </div>
+        <p class="text-justify">
+          After one year thinking about my future, I decided to sign up in
+          Telecommunications Engineering. My intention was to find out if I
+          really liked programming and IT, and I actually did. <br />During this
+          year I learned C, Mathlab and electronics fundamentals, and got honors
+          in Statistics, Programming (C) and Physics (Electronics).
+        </p>
+      </div>
+      <div id="s2-profile_picture-container" class="mx-auto">
+        <img src="/images/holder.jpeg" alt="gdm logo" />
+      </div>
+    </div>
+    <div
+      id="studies-3"
+      class="mb-16 grid grid-cols-2 gap-0 items-center bg-gray-700 shadow-l"
+    >
+      <div id="s3-profile_picture-container" class="mx-auto px-8">
+        <img src="/images/holder.jpeg" alt="ctag logo" />
+      </div>
+      <div id="s3-text-container" class="mx-6 py-8 text-gray-200">
+        <div id="s3-text-title" class="mb-8">
+          <h1 class="text-lime-400 text-4xl">Computer Engineering</h1>
+          <h3 class="text-2xl">2018 - 2022</h3>
+        </div>
+        <p class="text-justify">
+          In 2018 I decided to move to other town and started to study Computer
+          Engineering. I really enjoyed these years, and discovered my passion
+          as a developer.<br />I got honors in Programming (Java), Automatons &
+          Forming Languages and Digital Content Creation.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
