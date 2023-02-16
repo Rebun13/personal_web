3b3ddfb309c92b6ec5959e99f9e7ebb4308aebec
Rebun <55385643+Rebun13@users.noreply.github.com>
2023-02-16 12:11:05 +0000

    Projects page set
    "Work in progress" message.
    Ruben Gomez Dopazo

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="projects">
-    <h1>Projects</h1>
+  <div id="projects" class="lg:w-3/5 mx-auto mt-16 md:w-fullg">
+    <div id="projects-1" class="mb-16 items-center bg-gray-700 shadow-l">
+      <div id="e1-text-container" class="mx-6 py-8 text-gray-200 text-center">
+        <h1 class="text-4xl">Work in progress... 🛠️</h1>
+      </div>
+    </div>
   </div>
 </template>
